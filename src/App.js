922bcc8733e53f61174981c8f1ac8a4e8c621672
liannemoreno <lianne.moreno3@gmail.com>
2023-05-05 13:15:07 -0400

    added my other component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import Contact from "./components/Contact/contact.js"
+import Message from "./components/Message/message.js"
 // import Calendar from "./components/Calendar/calendar.js"
 // import Footer from "./components/Footer/Footer";
 // import Header from "./components/Header/Header";
---
+++ src/components/Message/message.js
@@ -0,0 +1,12 @@
+import "../Message/meontact.scss";
+
+
+function Contact() {
+    return (
+        <div className="message">
+            MESSAGE 
+        </div>
+    );
+}
+
+export default Contact;
