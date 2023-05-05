841c74d5c2945db63b3fa6d211ba2940655d83b2
liannemoreno <lianne.moreno3@gmail.com>
2023-05-05 14:06:57 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         {/* <Header /> */}
         {/* <Calendar /> */}
-        {/* <Message /> */}
+        {/* <Footer /> */}
       </BrowserRouter>
 
     </div>
