565a76e241ecb3903c90bf873072e13b5b535a8a
wfoncham <willfonchtv@gmail.com>
2023-05-05 11:06:32 -0400

    Testing the footer styling, added logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
+import React from "react";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +19,7 @@ function App() {
         >
           OLIVIA
         </a>
-      </header>
+      </header> */}
       <Footer />
     </div>
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,53 +1,56 @@
-import React from 'react';
+import React from "react";
+import "./Footer.scss";
+import footlogo from "../../../src/ukgplaceholderlogo.png";
+
 
 function Footer() {
-  return (
-    <footer>
-        <div classname="footer__logo">
-
-        </div>
-      <section className='footer'>
-        <div className='footer__first'>
-            <h1>Human Capital Management</h1>
-            <p>Human Resources</p>
-            <p>Payroll</p>
-            <p>Talent</p>
-            <p>Reporting and Analytics</p>   
-        </div>
-        <div className='footer__second'>
-            <h1>Workforce Management</h1>
-            <p>Time & Attendance</p>
-            <p>Scheduling</p>
-            <p>Compliance</p>  
-        </div>
-        <div className='footer__third'>
-            <h1>Product Suites</h1>
-            <p>UKG Pro</p>
-            <p>UKG Dimensions</p>
-            <p>UKG Ready</p>
-        </div>
-        <div className='footer__fourth'>
-            <h1>Customer Experience</h1>
-            <p>Implementation</p>
-            <p>Support Experience</p>
-            <p>UKG Community</p>
-            <p>Customer Stories</p>   
-        </div>
-        <div className='footer__fifth'>
-            <h1>About Us</h1>
-            <p>Why UKG</p>
-            <p>Culture</p>
-            <p>Our ESG Program</p>
-            <p>Leadership</p>  
-            <p>Newsroom</p>
-            <p>Careers</p>
-        </div>
-      </section>
-      <div classname="footer__icons">
-        
-      </div>
-    </footer>
-  );
+	return (
+		<footer>
+			<div classname="footer__up">
+                <img classname="footer__logo" src={footlogo} alt="" />
+            </div>
+			<section className="footer">
+				<div className="footer__block">
+					<h1>Human Capital Management</h1>
+					<p>Human Resources</p>
+					<p>Payroll</p>
+					<p>Talent</p>
+					<p>Reporting and Analytics</p>
+				</div>
+				<div className="footer__block">
+					<h1>Workforce Management</h1>
+					<p>Time & Attendance</p>
+					<p>Scheduling</p>
+					<p>Compliance</p>
+				</div>
+				<div className="footer__block">
+					<h1>Product Suites</h1>
+					<p>UKG Pro</p>
+					<p>UKG Dimensions</p>
+					<p>UKG Ready</p>
+				</div>
+				<div className="footer__block">
+					<h1>Customer Experience</h1>
+					<p>Implementation</p>
+					<p>Support Experience</p>
+					<p>UKG Community</p>
+					<p>Customer Stories</p>
+				</div>
+				<div className="footer__block">
+					<h1>About Us</h1>
+					<p>Why UKG</p>
+					<p>Culture</p>
+					<p>Our ESG Program</p>
+					<p>Leadership</p>
+					<p>Newsroom</p>
+					<p>Careers</p>
+				</div>
+			</section>
+			<div classname="footer__icons">
+                
+            </div>
+		</footer>
+	);
 }
 
 export default Footer;
