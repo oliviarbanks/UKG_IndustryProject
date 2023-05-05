bc107999178a0036845f30a17742d081c939479d
wfoncham <willfonchtv@gmail.com>
2023-05-05 13:24:41 -0400

    Trying to recreate Header in time.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,54 +1,19 @@
 import React from "react";
 import "./Footer.scss";
-import footlogo from "../../../src/ukgplaceholderlogo.png";
+import footerbar from "../../../src/assets/images/Tab Bar.png";
+
 
 
 function Footer() {
 	return (
 		<footer>
-			<div classname="footer__up">
-                <img classname="footer__logo" src={footlogo} alt="" />
+            <div className="footer__image">
+                <img src={footerbar} alt="" />
             </div>
 			<section className="footer">
-				<div className="footer__block">
-					<h1>Human Capital Management</h1>
-					<p>Human Resources</p>
-					<p>Payroll</p>
-					<p>Talent</p>
-					<p>Reporting and Analytics</p>
-				</div>
-				<div className="footer__block">
-					<h1>Workforce Management</h1>
-					<p>Time & Attendance</p>
-					<p>Scheduling</p>
-					<p>Compliance</p>
-				</div>
-				<div className="footer__block">
-					<h1>Product Suites</h1>
-					<p>UKG Pro</p>
-					<p>UKG Dimensions</p>
-					<p>UKG Ready</p>
-				</div>
-				<div className="footer__block">
-					<h1>Customer Experience</h1>
-					<p>Implementation</p>
-					<p>Support Experience</p>
-					<p>UKG Community</p>
-					<p>Customer Stories</p>
-				</div>
-				<div className="footer__block">
-					<h1>About Us</h1>
-					<p>Why UKG</p>
-					<p>Culture</p>
-					<p>Our ESG Program</p>
-					<p>Leadership</p>
-					<p>Newsroom</p>
-					<p>Careers</p>
-				</div>
-			</section>
-			<div classname="footer__icons">
-                
-            </div>
+
+            </section>
+			
 		</footer>
 	);
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,49 @@
 import React from "react";
+// import header from "../../../src/assets/images/Top Nav Bar.png";
+import "./Header.scss";
+import arrowimage from "../../../src/assets/images/Vector.png";
+import bellimage from "../../../src/assets/images/Bell.png";
 
-function Header () {
-    return (
-        <header>
-            <div classname="header">
-                <div className="header__logo">
-                    
+function Header() {
+	return (
+		<header>
+			<div classname="header">
+				{/* <div className="header__logo">
+                    <img className="header__bar" src={header} alt="" />
+                </div> */}
+                {/* <div className="header__top--section">
+
+                    </div> */}
+
+				<div className="header__top">
+                    <div className="header__arrow">
+						<img src={arrowimage} alt="" />
+					</div>
+					<div className="header__top--words">
+						<h1>Calendar</h1>
+					</div>
+					<div className="header__bell">
+						<img src={bellimage} alt="" />
+					</div>
                 </div>
-                <div classname="nav">
-                    
-                </div>
-                <div>
-                   
-                </div>  
-            </div>
-        </header>
-    );
+					<div className="header__bottom">
+						<div className="header__bottom--words">
+                            <div>
+                                <h2>My Calendar</h2>
+                            </div>
+							<div>
+                                <h2>Free Shifts</h2>
+                            </div>
+							<div>
+                                <h2>Time Off</h2>
+                            </div>
+							
+						</div>
+					</div>
+				
+			</div>
+		</header>
+	);
 }
 
 export default Header;
