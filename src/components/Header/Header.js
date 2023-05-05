55334e708aebb6fdabf521d0bcf97bf3651252ee
wfoncham <willfonchtv@gmail.com>
2023-05-05 12:22:51 -0400

    Redoing our header, importing images

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header () {
+    return (
+        <header>
+            <div classname="header">
+                <div className="header__logo">
+                    
+                </div>
+                <div classname="nav">
+                    
+                </div>
+                <div>
+                   
+                </div>  
+            </div>
+        </header>
+    );
+}
+
+export default Header;
